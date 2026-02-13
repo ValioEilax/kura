1148b559498fb8511a0f636f6103ead14daca9af
ValioEilax <119453927+ValioEilax@users.noreply.github.com>
2026-02-13 10:50:07 +0200

    Check user id in edit/remove

--- app.py
+++ app.py
@@ -48,6 +48,10 @@ def create_course():
 @app.route("/update_course", methods=["POST"])
 def update_course():
     course_id = request.form["course_id"]
+    course = courses.get_course(course_id)
+    if course["user_id"] != session["user_id"]:
+        abort(403)
+    
     name = request.form["name"]
     code = request.form["code"]
     credits = request.form["credits"]
@@ -61,13 +65,17 @@ def update_course():
 @app.route("/edit_course/<int:course_id>")
 def edit_course(course_id):
     course = courses.get_course(course_id)
+    if course["user_id"] != session["user_id"]:
+        abort(403)
     return render_template("edit_course.html", course=course)
 
 @app.route("/remove_course/<int:course_id>", methods=["GET", "POST"])
 def remove_course(course_id):
+    course = courses.get_course(course_id)
+    if course["user_id"] != session["user_id"]:
+        abort(403)
 
     if request.method == "GET":
-        course = courses.get_course(course_id)
         return render_template("remove_course.html", course=course)
     
     if request.method == "POST":
